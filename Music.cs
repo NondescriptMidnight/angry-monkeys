95b9836171c21d44d56768c8b156241449e3a419
NondescriptMidnight <59258519+NondescriptMidnight@users.noreply.github.com>
2019-12-26 17:31:43 +0200

    Add files via upload

---
+++ Music.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Music : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+        if (FindObjectsOfType(GetType()).Length > 1)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
+
